2e02f7d0848ba2350eef461bdb41fe106e4cef88
jmls9627 <jmls9627@gmail.com>
2020-06-04 14:20:28 -0400

    4.Generating an Angular Component

--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { BikeService } from './service/bike.service';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,

33b8a7cfce310cec9cdd2af384037c00fbfe3d47
jmls9627 <jmls9627@gmail.com>
2020-06-05 12:48:42 -0400

    5.Creating and Vaidating a Form

--- bike-ui/src/app/components/home/home.component.ts
+++ bike-ui/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BikeService } from 'src/app/service/bike.service';
+import{FormControl,FormGroup,Validators} from "@angular/forms"
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,28 @@ import { BikeService } from 'src/app/service/bike.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  models: string[]=[
+    'Globo MTB 29 Full Suspension',
+    'Globo Carbon Fiber Race Series',
+    'Globo Time Trial Blade',
+];
+   bikeForm: FormGroup;
+   validMessage: string="";
 
   constructor(private bikeServ:BikeService) { }
 
   ngOnInit() {
+    this.bikeForm=new FormGroup({
+      name: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.required),
+      phone: new FormControl('',Validators.required),
+      model: new FormControl('',Validators.required),
+      serialNumber: new FormControl('',Validators.required),
+      purchasePrice: new FormControl('',Validators.required),
+      purchaseDate: new FormControl('',Validators.required),
+      contact: new FormControl()
+      
+    });
   }
 
 }
